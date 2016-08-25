bf230b0dd9862aaaece5b8c8673cdc7289268c0e
seal <hasanat.muztaba@gmail.com>
2016-08-26 04:42:27 +0600

    bug can not find :(

--- src/main/java/com/muztaba/App.java
+++ src/main/java/com/muztaba/App.java
@@ -7,17 +7,25 @@ import com.muztaba.service.entity.VerdictService;
 import com.muztaba.service.compiler.Compiler;
 import com.muztaba.service.producer.DummyProblem;
 import com.muztaba.service.queue.QueueService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.Future;
+
 /**
  * Hello world!
- *
  */
 @Component
 public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     @Autowired
     QueueService<Submission> queue;
@@ -31,18 +39,56 @@ public class App {
     @Autowired
     DummyProblem dummyProblem; //false data insert to problem table
 
-    public static void main( String[] args ) {
+    @Autowired
+    CompletionService<Verdict> completionService;
+
+    public static void main(String[] args) {
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         App app = context.getBean(App.class);
         app.task();
     }
 
+    boolean first = true;
+
     private void task() {
+        int num = Runtime.getRuntime().availableProcessors() + 1;
         while (true) {
-            if (!queue.isEmpty()) {
+
+            while (first) {
+                List<Submission> list = queue.getList(num);
+                if (list.size() != 0) {
+                    first = false;
+                    for (Submission s : list) {
+                        completionService.submit(new Callable<Verdict>() {
+                            @Override
+                            public Verdict call() throws Exception {
+                                return compiler.submit(s);
+                            }
+                        });
+                    }
+                }
+            }
+
+            if (!first & !queue.isEmpty()) {
                 Submission submission = queue.get();
-                Verdict verdict = compiler.submit(submission);
-                verdictService.post(verdict);
+                logger.info("Add to thread, {}", submission);
+                completionService.submit(new Callable<Verdict>() {
+                    @Override
+                    public Verdict call() throws Exception {
+                        return compiler.submit(submission);
+                    }
+                });
+            }
+
+            try {
+                Future<Verdict> futureVerdict = completionService.take();
+                Verdict verdict = futureVerdict.get();
+                if (verdict != null) {
+                    logger.info("Verdict Save {}", verdict);
+                    verdictService.post(verdict);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
--- src/main/java/com/muztaba/config/AppConfig.java
+++ src/main/java/com/muztaba/config/AppConfig.java
@@ -1,9 +1,15 @@
 package com.muztaba.config;
 
+import com.muztaba.entity.Verdict;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by seal on 8/13/2016.
@@ -13,4 +19,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages = "com.muztaba")
 @PropertySource(value = "classpath:application.properties")
 public class AppConfig {
+
+    @Bean
+    public ExecutorCompletionService<Verdict> getBean() {
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
+        return new ExecutorCompletionService<>(executorService);
+    }
 }
