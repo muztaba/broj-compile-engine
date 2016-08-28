853eb27dfa43f8a071cd6c916f1742817f0183a5
Muztaba Hasanat <hasanat.muztaba@gmail.com>
2016-08-28 22:18:19 +0600

    Verdict entity change

--- src/main/java/com/muztaba/entity/Verdict.java
+++ src/main/java/com/muztaba/entity/Verdict.java
@@ -7,12 +7,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by seal on 8/20/16.
  */
 @Entity
-@Table(name = "verdict")
+@Table(name = "verdicts")
 @Data
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
@@ -24,10 +25,34 @@ public class Verdict {
     private long id;
 
     @OneToOne(targetEntity = Submission.class)
-    @JoinColumn(name = "submission", referencedColumnName = "submission_id")
+    @JoinColumn(name = "submission_id", referencedColumnName = "submission_id")
     private Submission submission;
 
-    @Column(name = "verdict")
+    @Column(name = "output_file", length = 100000)
+    private byte[] outputFile;
+
+    @Column(name = "output_file_special", length = 100000)
+    private byte[] outputFileSpecial;
+
+    @Column(name = "result")
     @Enumerated(EnumType.STRING)
     private CompileStatus compileStatus;
+
+    @Column(name = "execution_time")
+    private Long executionTime;
+
+    @Column(name = "execution_memory")
+    private Long executionMemory;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+
+    @Column(name = "remember_token")
+    private String rememberToken;
+
 }
